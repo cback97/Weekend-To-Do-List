b5cb8d5c370294603a5f51822684171d297a4e04
Christian Back <cdirt27@gmail.com>
2021-04-26 01:36:28 -0500

    Establised router file, set up router module export

---
+++ server/routes/todorouter.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const {Router} = require('express');
+const ToDoRouter = express.Router();
+
+
+
+
+// Connect to Database w/ Postgres
+const pg = require('pg');
+const pool = new pg.Pool(config)
+
+const config = {
+    database:'weekend-to-do-app',
+    host: 'localhost',
+    port: '5432',
+    max: '10',
+    idleTimeoutMillis: 30000,
+}
+
+
+module.exports = ToDoRouter;
