519cfa63a4c982e7670cdfa7a9827d5d2a618cb9
Christian Back <cdirt27@gmail.com>
2021-04-26 02:32:14 -0500

    built get route for grabbing database info

--- server/routes/todorouter.js
+++ server/routes/todorouter.js
@@ -18,6 +18,15 @@ const config = {
 }
 
 // GET
+ToDoRouter.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "todo-list" ORDER BY "id";'
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    }).catch(error => {
+        console.log('error getting to-do list', error);
+        res.sendStatus(500);
+    });
+});
 
 // POST
 
